d137c4734547810b99701c0e59d75ddb1b1b5906
hyungmin.oh <hyungmin.oh@nhn.com>
2026-01-08 15:37:50 +0900

    Update nhncloud-cli to v0.6.0
    New features:
    - auth command (status, token refresh/revoke)
    - ssh-keys management (import, export, remove, use)
    - object-storage cp/mv/sync (AWS CLI-like file ops)
    - --query flag for JMESPath filtering
    - RDS cert management, connect, query commands
    - MariaDB/PostgreSQL instance-groups support

--- Formula/nhncloud-cli.rb
+++ Formula/nhncloud-cli.rb
@@ -1,17 +1,24 @@
 class NhncloudCli < Formula
   desc "Command line interface for NHN Cloud services"
   homepage "https://github.com/haung921209/nhn-cloud-cli"
-  url "https://github.com/haung921209/nhn-cloud-cli/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "3026a81d0844d25046b573c8a24f3a167237149a6a87f4205da5fdcfec4ca498"
+  url "https://github.com/haung921209/nhn-cloud-cli/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "323b7a7fd6e79d2e16e6139b0d59d063f5a822031eaa2ca59603832f1d60bc93"
   license "Apache-2.0"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "-o", bin/"nhncloud", "."
+    ldflags = %W[
+      -s -w
+      -X github.com/haung921209/nhn-cloud-cli/cmd.Version=#{version}
+      -X github.com/haung921209/nhn-cloud-cli/cmd.GitCommit=#{tap.user}
+      -X github.com/haung921209/nhn-cloud-cli/cmd.BuildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "-o", bin/"nhncloud", "."
   end
 
   test do
     assert_match "NHN Cloud CLI", shell_output("#{bin}/nhncloud --help")
+    assert_match version.to_s, shell_output("#{bin}/nhncloud version")
   end
 end
