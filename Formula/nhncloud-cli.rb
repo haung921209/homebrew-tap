e20fc990a49f64cf8ad13f831de8221fbd285af3
hyungmin.oh <hyungmin.oh@nhn.com>
2026-01-20 10:20:39 +0900

    update nhncloud-cli to v0.7.6

--- Formula/nhncloud-cli.rb
+++ Formula/nhncloud-cli.rb
@@ -1,8 +1,8 @@
 class NhncloudCli < Formula
   desc "Command line interface for NHN Cloud services"
   homepage "https://github.com/haung921209/nhn-cloud-cli"
-  url "https://github.com/haung921209/nhn-cloud-cli/archive/refs/tags/v0.7.5.tar.gz"
-  sha256 "4e13359320437ced4fb46cb4468d17eee21aa73a6218c79fa252841dc26c5320"
+  url "https://github.com/haung921209/nhn-cloud-cli/archive/refs/tags/v0.7.6.tar.gz"
+  sha256 "c325102fcb75accb629f79974a6045d3377133582eb9d48f9d44ffd2e739ce22"
   license "Apache-2.0"
 
   depends_on "go" => :build
